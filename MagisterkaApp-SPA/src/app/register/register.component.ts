8f7a2cbc78e81f5af5d6156dc5b0023180755a71
Rafal Leszczewski <rleszczewski@gmail.com>
2018-09-09 19:55:47 +0200

    Communication with Api

---
+++ MagisterkaApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  // 997 we got acces to valuesFromHome inside component
+  // which got all values that is passed out from home component
+ // @Input() valuesFromHome: any;
+
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
--- MagisterkaApp.API/Controllers/ValuesController.cs
+++ MagisterkaApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace MagisterkApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
